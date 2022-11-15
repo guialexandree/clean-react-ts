bb47006cf83945e68f11919eeb47a5c4f512a1c6
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2022-11-15 18:51:01 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -2,15 +2,28 @@ import { RemoteAuthentication } from './remote-authentication'
 import { HttpPostClientSpy } from '@/data/test/mocks'
 import faker from 'faker'
 
+type SutTypes = {
+  sut: RemoteAuthentication
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const sut = new RemoteAuthentication(
+    url,
+    httpPostClientSpy
+  )
+
+  return {
+    sut,
+    httpPostClientSpy
+  }
+}
+
 describe('RemoteAuthentication', () => {
   test('Should call httpCliente if correct url', async () => {
     const url = faker.internet.url()
-    const httpPostClientSpy = new HttpPostClientSpy()
-    const sut = new RemoteAuthentication(
-      url,
-      httpPostClientSpy
-    )
-
+    const { sut, httpPostClientSpy } = makeSut(url)
     await sut.auth()
 
     expect(httpPostClientSpy.url).toBe(url)
