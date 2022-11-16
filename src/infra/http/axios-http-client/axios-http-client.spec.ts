878658fd168e9cae1c453c9051fa04ee6b823094
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2022-11-16 20:46:48 -0300

    test: ensure AxiosHttpClient calls axios with correct values

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -10,12 +10,12 @@ const makeSut = (): AxiosHttpClient => {
 }
 
 describe('AxiosHttpClient', () => {
-  test('Should call axios with correct url', async () => {
+  test('Should call axios with correct values', async () => {
     const sut = makeSut()
 
     const url = faker.internet.url()
     await sut.post({ url })
 
-    expect(mockedAxios).toHaveBeenCalledWith(url)
+    expect(mockedAxios.post).toHaveBeenCalledWith(url)
   })
 })
