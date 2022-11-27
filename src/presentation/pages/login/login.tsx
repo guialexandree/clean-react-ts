6b12fd8cc1ab1617d3cc1981f20c81abfe2d12ea
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2022-11-27 18:43:48 -0300

    feat: ensure Login disable submit button only if form is invalid

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -36,7 +36,7 @@ const Login: React.FC<LoginProps> = ({
 					<h2>Login</h2>
 					<Input type="email" name="email" placeholder="Digite seu e-mail" />
 					<Input type="password" name="password" placeholder="Digite sua senha" />
-					<button data-testid='submit' disabled className={S.submit} type="submit">Entrar</button>
+					<button data-testid='submit' disabled={!!state.emailError || !!state.passwordError} className={S.submit} type="submit">Entrar</button>
 					<span className={S.link}>Criar conta</span>
 					<FormStatus />
 				</form>
