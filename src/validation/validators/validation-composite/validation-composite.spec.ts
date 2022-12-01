73762b1139ea2d97372bbbcbe9c0e1e491bbc478
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2022-11-30 22:41:52 -0300

    test: ensure ValidationComposite returns the first error found

---
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -0,0 +1,19 @@
+import { FieldValidationSpy } from '@/validation/test'
+import { ValidationComposite } from './validation-composite'
+
+describe('ValidationComposite', () => {
+  test('Should return erro if any validation fails', () => {
+    const fieldValidationSpy = new FieldValidationSpy('any_field')
+    fieldValidationSpy.error = new Error('first_error')
+    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
+    fieldValidationSpy2.error = new Error('second_error')
+
+    const sut = new ValidationComposite([
+      fieldValidationSpy,
+      fieldValidationSpy2
+    ])
+
+    const error = sut.validate('any_field', 'any_value')
+    expect(error).toBe('first_error')
+  })
+})
