864a66fa1af4ffef39e9547d0cefcdb591b859ec
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2022-12-02 22:24:41 -0300

    test: ensure ValidationComposite uses correct validations

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -4,10 +4,10 @@ import { makeLoginValidation } from '@/main/factories/validation'
 import { makeRemoteAuthentication } from '@/main/factories/usecases'
 
 export const makeLogin: React.FC = () => {
-	return (
-		<Login 
-			authentication={makeRemoteAuthentication()} 
-			validation={makeLoginValidation()} 
+  return (
+		<Login
+			authentication={makeRemoteAuthentication()}
+			validation={makeLoginValidation()}
 		/>
-	)
+  )
 }
--- src/presentation/router/router.tsx
+++ src/presentation/router/router.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import {  BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 type Props = {
-	makeLogin: React.FC
+  makeLogin: React.FC
 }
 
 const Router: React.FC<Props> = ({ makeLogin }: Props) => {
