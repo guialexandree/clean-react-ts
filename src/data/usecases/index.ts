a895d0a1fe7d9dd7807109f057b89f12461b831d
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2022-12-04 18:14:16 -0300

    feat: ensure LocalStorageAccessToken calls SetStorage with correct value

--- src/data/usecases/index.ts
+++ src/data/usecases/index.ts
@@ -1,1 +1,2 @@
 export * from './authentication/remote-authentication'
+export * from './save-access-token/save-access-token'
---
+++ src/data/usecases/save-access-token/save-access-token.ts
@@ -0,0 +1,10 @@
+import { SaveAccessToken } from '@/domain/usecases/save-access-token'
+import { SetStorage } from '@/data/protocols'
+
+export class LocalSaveAccessToken implements SaveAccessToken {
+  constructor (private readonly setStorage: SetStorage) {}
+
+  async save (accessToken: string): Promise<void> {
+    await this.setStorage.set('accessToken', accessToken)
+  }
+}
