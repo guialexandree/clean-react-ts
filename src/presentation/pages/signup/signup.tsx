5246cbbd8691b0e9fb6f698393bc5a542c7e93d2
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2022-12-05 22:31:48 -0300

    feat: ensure SignUp hide spinner and present error if AddAccount fails

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -38,15 +38,23 @@ const SignUp: React.FC<SignUpProps> = ({
   }, [state.name, state.email, state.password, state.passwordConfirmation])
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
-    event.preventDefault()
-    if (state.isLoading || state.nameError || state.emailError || state.passwordError || state.passwordConfirmationError) { return }
-    setState({ ...state, isLoading: true })
-    await addAccount.add({
-      name: state.name,
-      email: state.email,
-      password: state.password,
-      passwordConfirmation: state.passwordConfirmation
-    })
+		event.preventDefault()
+		try {
+			if (state.isLoading || state.nameError || state.emailError || state.passwordError || state.passwordConfirmationError) { return }
+			setState({ ...state, isLoading: true })
+			await addAccount.add({
+				name: state.name,
+				email: state.email,
+				password: state.password,
+				passwordConfirmation: state.passwordConfirmation
+			})
+		} catch (error) {
+			setState({
+				...state,
+				isLoading: false,
+				mainError: error.message
+			})
+		}
   }
 
   return (
