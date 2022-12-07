f5b22df1278deedd19cbae2d3f86c824abaabc9b
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2022-12-07 19:21:13 -0300

    feat: add SignUpValidationFactory

---
+++ src/main/factories/validation/signup-validation-factory.ts
@@ -0,0 +1,9 @@
+import { ValidationComposite } from '@/validation/validators'
+import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder'
+
+export const makeSignUpValidation = (): ValidationComposite => new ValidationComposite([
+  ...Builder.field('name').required().min(5).build(),
+  ...Builder.field('email').required().email().build(),
+  ...Builder.field('password').required().min(5).build(),
+  ...Builder.field('passwordConfirmation').required().min(5).build()
+])
--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -33,10 +33,12 @@ const SignUp: React.FC<SignUpProps> = ({
   })
 
   useEffect(() => {
-    const nameError = validation.validate('name', state.name)
-    const emailError = validation.validate('email', state.email)
-    const passwordError = validation.validate('password', state.passwordError)
-    const passwordConfirmationError = validation.validate('passwordConfirmation', state.passwordError)
+    const { name, email, password, passwordConfirmation } = state
+    const formData = { name, email, password, passwordConfirmation }
+    const nameError = validation.validate('name', formData)
+    const emailError = validation.validate('email', formData)
+    const passwordError = validation.validate('password', formData)
+    const passwordConfirmationError = validation.validate('passwordConfirmation', formData)
 
     setState({
       ...state,
