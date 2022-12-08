e9e2631f337a0895d51ba1cc7de37b83f23c954a
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2022-12-07 22:28:44 -0300

    test: ensure Login present erro ifg state if form is invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,3 +1,5 @@
+import faker from 'faker'
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
@@ -10,6 +12,25 @@ describe('Login', () => {
     cy
       .getByTestId('password-status')
       .should('have.attr', 'title', 'Campo Obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present erro if form is invalid', () => {
+    cy
+      .getByTestId('email')
+      .type(faker.random.word())
+    cy
+      .getByTestId('password')
+      .type(faker.datatype.string(4))
+    cy
+      .getByTestId('email-status')
+      .should('have.attr', 'title', 'Valor inválido')
+      .should('contain.text', '🔴')
+    cy
+      .getByTestId('password-status')
+      .should('have.attr', 'title', 'Valor inválido')
       .should('contain.text', '🔴')
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
