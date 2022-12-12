49cdfd9f1ccaad75f8ba583f92f763e6ed60a598
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2022-12-12 18:55:41 -0300

    test: ensure LocalStorageAdapter calls localStorage.getItem with correct value

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -1,6 +1,7 @@
 import 'jest-localstorage-mock'
 import { LocalStorageAdapter } from './local-storage-adapter'
 import faker from 'faker'
+import { AccountModel } from '@/domain/models'
 
 const makeSut = (): LocalStorageAdapter => new LocalStorageAdapter()
 
@@ -9,11 +10,21 @@ describe('LocalStorage Adapter', () => {
     localStorage.clear()
   })
 
-  test('Should call SetLocalStorage with correct values', async () => {
+  test('Should call localSotrage.setItem with correct values', async () => {
     const sut = makeSut()
     const key = faker.database.column()
-    const value = faker.random.objectElement<{}>()
+    const value = faker.random.objectElement<AccountModel>()
     sut.set(key, value)
     expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value))
+  })
+
+  test('Should return an account on localSotrage.getItem with correct value', async () => {
+    const sut = makeSut()
+    const key = faker.database.column()
+    const value = faker.random.objectElement<AccountModel>()
+    jest.spyOn(localStorage, 'getItem').mockReturnValueOnce(JSON.stringify(value))
+    const obj = sut.get(key)
+    expect(obj).toEqual(value)
+    expect(localStorage.getItem).toHaveBeenCalledWith(key)
   })
 })
