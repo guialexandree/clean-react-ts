e39997242f0e0281d8193e20ea5c9cdd6f4a5524
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2022-12-21 11:19:56 -0300

    feat: ensure Logout calls setCurrentAccount with undefined

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -1,17 +1,26 @@
-import React, { memo } from 'react'
 import { Logo } from '@/presentation/components'
 import S from './header-styles.scss'
+import { ApiContext } from '@/presentation/contexts'
+import { useHistory } from 'react-router-dom'
+import React, { memo, useContext } from 'react'
 
-type HeaderProps = { }
+const Header: React.FC = () => {
+	const history = useHistory()
+	const { setCurrentAccount } = useContext(ApiContext)
 
-const Header: React.FC<HeaderProps> = () => {
+	const logout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
+		event.preventDefault()
+		setCurrentAccount(undefined)
+		history.replace('/login')
+	}
+
   return (
 		<header className={S.headerWrap}>
 				<section className={S.headerContent}>
 				<Logo />
 				<section className={S.logoutWrap}>
 					<span>Guilherme</span>
-					<a href="#">Sair</a>
+					<a data-testid="logout" href="#" onClick={logout}>Sair</a>
 				</section>
 				</section>
 		</header>
