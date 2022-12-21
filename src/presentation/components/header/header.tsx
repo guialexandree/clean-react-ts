24234ca40f2e2610352b729a0ae63f3b68c61346
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2022-12-21 11:41:02 -0300

    feat: ensure header render username corretly

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -6,7 +6,7 @@ import React, { memo, useContext } from 'react'
 
 const Header: React.FC = () => {
   const history = useHistory()
-  const { setCurrentAccount } = useContext(ApiContext)
+  const { setCurrentAccount, getCurrentAccount } = useContext(ApiContext)
 
   const logout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
     event.preventDefault()
@@ -19,7 +19,7 @@ const Header: React.FC = () => {
 				<section className={S.headerContent}>
 				<Logo />
 				<section className={S.logoutWrap}>
-					<span>Guilherme</span>
+					<span data-testid="username">{getCurrentAccount().name}</span>
 					<a data-testid="logout" href="#" onClick={logout}>Sair</a>
 				</section>
 				</section>
