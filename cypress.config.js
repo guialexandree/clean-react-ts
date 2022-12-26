b77239487e43767d52afc046d0a1ad5cd5828e27
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2022-12-26 17:14:24 -0300

    chore: config travis ci to use cypress

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   fixturesFolder: 'src/main/test/cypress/fixtures',
   viewportHeight: 860,
+	video: false,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
