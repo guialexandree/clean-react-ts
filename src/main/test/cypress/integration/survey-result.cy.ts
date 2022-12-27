b713ce16621c4942d394a1234681696681e9ba7d
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2022-12-27 19:21:03 -0300

    test: ensure SurveyREsult back to SurveyList on back button click

--- src/main/test/cypress/integration/survey-result.cy.ts
+++ src/main/test/cypress/integration/survey-result.cy.ts
@@ -52,4 +52,13 @@ describe('SurveyResult', () => {
       assert.notExists(li.find('[data-testid="image"]'))
     })
   })
+
+  it('Should goto SurveyList on back button click', () => {
+    mockSuccess()
+    cy.visit('')
+    cy.visit('/surveys/any_id')
+    cy.getByTestId('back-button').click().then(() => {
+      Helpers.testUrl('/')
+    })
+  })
 })
