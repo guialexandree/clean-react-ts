c818d80018c10008a04049e6bd3e7861c5974072
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2022-12-27 12:31:31 -0300

    refactor: change surveyResut to return isCurrentAccountAnswer

--- src/data/test/mocks/mock-remote-survey-result.ts
+++ src/data/test/mocks/mock-remote-survey-result.ts
@@ -9,11 +9,13 @@ export const mockRemoteSurveyResultModel = (): RemoteLoadSurveyResult.Model => (
       image: faker.internet.url(),
       answer: faker.random.word(),
       count: faker.datatype.number(),
-      percent: faker.datatype.number({ max: 100 })
+      percent: faker.datatype.number({ max: 100 }),
+      isCurrentAccountAnswer: faker.datatype.boolean()
     }, {
       answer: faker.random.word(),
       count: faker.datatype.number(),
-      percent: faker.datatype.number({ max: 100 })
+      percent: faker.datatype.number({ max: 100 }),
+      isCurrentAccountAnswer: faker.datatype.boolean()
     }
   ]
 })
