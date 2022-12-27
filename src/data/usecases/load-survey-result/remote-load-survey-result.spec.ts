2140bb45a95232bb265d9fca48a3e7d7da6655a3
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2022-12-27 12:02:18 -0300

    test: ensure RemoteLoadSurveyResult call httpCliente with correct url

---
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -0,0 +1,27 @@
+import { RemoteLoadSurveyResult } from '@/data/usecases'
+import { HttpGetClientSpy } from '@/data/test/mocks'
+import faker from 'faker'
+
+type SutTypes = {
+  sut: RemoteLoadSurveyResult
+  httpGetClientSpy: HttpGetClientSpy
+}
+
+const makeSut = (url = faker.internet.url()): SutTypes => {
+  const httpGetClientSpy = new HttpGetClientSpy()
+  const sut = new RemoteLoadSurveyResult(url, httpGetClientSpy)
+
+  return {
+    sut,
+    httpGetClientSpy
+  }
+}
+
+describe('RemoteLoadSurveyResult', () => {
+  test('Should call HttpGetClient with correct url', async () => {
+    const url = faker.internet.url()
+    const { sut, httpGetClientSpy } = makeSut(url)
+    await sut.load()
+    expect(httpGetClientSpy.url).toBe(url)
+  })
+})
