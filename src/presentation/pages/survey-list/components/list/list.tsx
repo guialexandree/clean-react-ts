ceff726da06e229611ec5b26042c7f4880ba96ec
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2022-12-27 15:01:43 -0300

    refactor: remove unnecessary Context

--- src/presentation/pages/survey-list/components/list/list.tsx
+++ src/presentation/pages/survey-list/components/list/list.tsx
@@ -1,13 +1,16 @@
-import { LoadSurveyList } from '@/domain/usecases'
-import { SurveyItemEmpty, SurveyItem, SurveyContext } from '@/presentation/pages/survey-list/components'
+import React from 'react'
 import S from './list-styles.scss'
-import React, { useContext } from 'react'
+import { LoadSurveyList } from '@/domain/usecases'
+import { SurveyItemEmpty, SurveyItem } from '@/presentation/pages/survey-list/components'
 
-const List: React.FC = () => {
-  const { state } = useContext(SurveyContext)
+type Props = {
+	surveys: LoadSurveyList.Model[]
+}
+
+const List: React.FC<Props> = ({ surveys }) => {
   return (
 		<ul className={S.listWrap} data-testid='survey-list'>
-			{state.surveys.length ?	state.surveys.map((survey: LoadSurveyList.Model) => <SurveyItem key={survey.id} survey={survey} />) :	<SurveyItemEmpty />}
+			{surveys.length ?	surveys.map((survey: LoadSurveyList.Model) => <SurveyItem key={survey.id} survey={survey} />) :	<SurveyItemEmpty />}
 		</ul>
   )
 }
--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,6 +1,6 @@
 import Styles from './survey-list-styles.scss'
 import { Header, Footer, Error } from '@/presentation/components'
-import { SurveyContext, SurveyListItem } from '@/presentation/pages/survey-list/components'
+import { SurveyListItem } from '@/presentation/pages/survey-list/components'
 import { useErrorHandler } from '@/presentation/hooks'
 import { LoadSurveyList } from '@/domain/usecases'
 import React, { useEffect, useState } from 'react'
@@ -29,20 +29,16 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
     setState(old => ({ surveys: [], error: '', reload: !old.reload }))
   }
 
-  return (
-    <div className={Styles.surveyListWrap}>
-      <Header />
-      <div className={Styles.contentWrap}>
-        <h2>Enquetes</h2>
-        <SurveyContext.Provider value={{ state, setState }}>
-          {state.error
-            ? <Error error={state.error} reload={reload} />
-            : <SurveyListItem />}
-        </SurveyContext.Provider>
-      </div>
-      <Footer />
-    </div>
-  )
+	return (
+		<div className={Styles.surveyListWrap}>
+			<Header />
+			<div className={Styles.contentWrap}>
+				<h2>Enquetes</h2>
+				{state.error ?	<Error error={state.error} reload={reload} /> :	<SurveyListItem surveys={state.surveys} />}
+			</div>
+			<Footer />
+		</div>
+	)
 }
 
 export default SurveyList
