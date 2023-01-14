7a8b8957143b796260e36f116b0ff0e8ca049306
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-14 12:12:14 -0300

    test: ensure SurveyResult logout on AccessDeniedError

--- src/main/test/cypress/integration/survey-result.cy.ts
+++ src/main/test/cypress/integration/survey-result.cy.ts
@@ -70,6 +70,7 @@ describe('SurveyResult', () => {
 
   describe('save', () => {
     const mockUnexpectedError = (): void => Http.mockServerError(path, 'PUT')
+    const mockAccessDeniedError = (): void => Http.mockForbiddenError(path, 'PUT')
 
     beforeEach(() => {
       cy.fixture('account').then(account => {
@@ -83,6 +84,12 @@ describe('SurveyResult', () => {
       mockUnexpectedError()
       cy.get('li:nth-child(2)').click()
       cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu, Tente novamente em breve.')
+    })
+
+    it('Should logout on AccessDeniedError', () => {
+      mockAccessDeniedError()
+      cy.get('li:nth-child(2)').click()
+      Helpers.testUrl('/login')
     })
   })
 })
