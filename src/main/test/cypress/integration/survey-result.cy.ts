3a2a64b8548d3375cf54c968a8331c7d56175c4d
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-14 12:19:47 -0300

    test: ensure SurveyResult present new result on answer click

--- src/main/test/cypress/integration/survey-result.cy.ts
+++ src/main/test/cypress/integration/survey-result.cy.ts
@@ -4,7 +4,7 @@ import * as Http from '../utils/http-mocks'
 const path = 'api/surveys/any_id/results'
 export const mockLoadSuccess = (): void => {
   Http.mockOk('api/surveys', 'GET', 'survey-list')
-  Http.mockOk(path, 'GET', 'survey-result')
+  Http.mockOk(path, 'GET', 'load-survey-result')
 }
 
 describe('SurveyResult', () => {
@@ -71,6 +71,10 @@ describe('SurveyResult', () => {
   describe('save', () => {
     const mockUnexpectedError = (): void => Http.mockServerError(path, 'PUT')
     const mockAccessDeniedError = (): void => Http.mockForbiddenError(path, 'PUT')
+    const mockSaveSuccess = (): void => {
+      Http.mockOk('api/surveys', 'GET', 'survey-list')
+      Http.mockOk(path, 'PUT', 'save-survey-result')
+    }
 
     beforeEach(() => {
       cy.fixture('account').then(account => {
@@ -90,6 +94,25 @@ describe('SurveyResult', () => {
       mockAccessDeniedError()
       cy.get('li:nth-child(2)').click()
       Helpers.testUrl('/login')
+    })
+
+    it('Should present survey result', () => {
+      mockSaveSuccess()
+      cy.get('li:nth-child(2)').click()
+      cy.getByTestId('question').should('have.text', 'Other Question')
+      cy.getByTestId('day').should('have.text', '21')
+      cy.getByTestId('month').should('have.text', 'dez')
+      cy.getByTestId('year').should('have.text', '2023')
+      cy.get('li:nth-child(1)').then(li => {
+        assert.equal(li.find('[data-testid="answer"]').text(), 'other_answer')
+        assert.equal(li.find('[data-testid="percent"]').text(), '50%')
+        assert.equal(li.find('[data-testid="image"]').attr('src'), 'other_image')
+      })
+      cy.get('li:nth-child(2)').then(li => {
+        assert.equal(li.find('[data-testid="answer"]').text(), 'other_answer_2')
+        assert.equal(li.find('[data-testid="percent"]').text(), '50%')
+        assert.notExists(li.find('[data-testid="image"]'))
+      })
     })
   })
 })
