d4f5a9be3ae2076edf42e69993e1a83fcb2d1e7b
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-02-12 12:38:29 -0300

    fix: remove vulnerability x-powered-by header in express app

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fallback = require('express-history-api-fallback')
 const express = require('express')
 const app = express()
+app.disable('x-powered-by');
 const root = `${__dirname}/dist`
 app.use(express.static(root))
 app.use(fallback('index.html', { root }))
